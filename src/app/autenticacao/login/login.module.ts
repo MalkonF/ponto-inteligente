9c4461c278e058e337e3be1fea631de876f43ed4
Malkon Faria <sudo777@protonmail.com>
2020-12-16 20:31:05 -0300

    Criacao do component login / Importacao do component no login.module

--- src/app/autenticacao/login/login.module.ts
+++ src/app/autenticacao/login/login.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [CommonModule],
 })
 export class LoginModule {}
