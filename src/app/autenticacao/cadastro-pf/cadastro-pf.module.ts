2baa77e4bd4a2996a1a63dd6a678f98848fb7b79
Malkon Faria <sudo777@protonmail.com>
2021-01-27 03:19:21 -0300

    Criado form e model cadastro pf

--- src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
+++ src/app/autenticacao/cadastro-pf/cadastro-pf.module.ts
@@ -3,9 +3,33 @@ import { CommonModule } from '@angular/common';
 import { CadastrarPfComponent, CadastroPfComponent } from './components';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [CadastrarPfComponent, CadastroPfComponent],
-  imports: [CommonModule, FlexLayoutModule, RouterModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    SharedModule,
+  ],
 })
 export class CadastroPfModule {}
--- src/app/autenticacao/cadastro-pf/components/cadastrar-pf/cadastrar-pf.component.ts
+++ src/app/autenticacao/cadastro-pf/components/cadastrar-pf/cadastrar-pf.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+import { CpfValidator, CnpjValidator } from '../../../../shared/validators';
+
+import { CadastroPf } from '../../models';
 
 @Component({
   selector: 'app-cadastrar-pf',
   templateUrl: './cadastrar-pf.component.html',
-  styleUrls: ['./cadastrar-pf.component.css']
+  styleUrls: ['./cadastrar-pf.component.css'],
 })
 export class CadastrarPfComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.gerarForm();
   }
 
+  gerarForm() {
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      cpf: ['', [Validators.required, CpfValidator]],
+      cnpj: ['', [Validators.required, CnpjValidator]],
+    });
+  }
+
+  cadastrarPf() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const cadastroPf: CadastroPf = this.form.value;
+    alert(JSON.stringify(cadastroPf));
+
+    return false;
+  }
 }
